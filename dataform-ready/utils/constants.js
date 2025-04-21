6825fdfe7600b169fbde2b91fcf17be975f57f0a
chriswalt <chriswalt@hotmail.co.uk>
2025-04-21 12:45:59 +0200

    Finalised Dataform setup with constants, dynamic vars, and placeholder project settings

--- dataform-ready/utils/constants.js
+++ dataform-ready/utils/constants.js
@@ -1,1 +1,27 @@
-// placeholder
+// utils/constants.js
+
+/**
+ * Project-wide constants and config options.
+ * These are optional reference lists used in assertions and reports.
+ */
+
+module.exports = {
+    knownValidBrowsers: [
+      "Chrome",
+      "Safari",
+      "Firefox",
+      "Edge",
+      "Samsung Internet",
+      "Internet Explorer"
+    ],
+    requiredEcommerceParams: [
+      "transaction_id",
+      "item_id",
+      "item_name",
+      "currency",
+      "price",
+      "quantity"
+    ],
+    allowedConsentStates: ["granted", "denied", "unset"]
+  };
+  
